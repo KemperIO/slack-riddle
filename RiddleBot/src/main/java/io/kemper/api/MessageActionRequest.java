d3f8982fdb5e77213650921a9882929292d293cb
Taylor Kemper <tjkemper6@gmail.com>
2017-05-28 02:18:07 -0400

    remove unneeded files

--- RiddleBot/src/main/java/io/kemper/api/MessageActionRequest.java
+++ RiddleBot/src/main/java/io/kemper/api/MessageActionRequest.java
@@ -5,20 +5,10 @@ import java.util.List;
 public class MessageActionRequest {
     private List<Action> actions;
 
-    private String callbackId;
-
-    private Team team;
-    private Channel channel;
-    private User user;
-
     public MessageActionRequest(){}
 
-    public MessageActionRequest(List<Action> actions, String callbackId, Team team, Channel channel, User user) {
+    public MessageActionRequest(List<Action> actions) {
         this.actions = actions;
-        this.callbackId = callbackId;
-        this.team = team;
-        this.channel = channel;
-        this.user = user;
     }
 
     public List<Action> getActions() {
@@ -29,38 +19,6 @@ public class MessageActionRequest {
         this.actions = actions;
     }
 
-    public String getCallbackId() {
-        return callbackId;
-    }
-
-    public void setCallbackId(String callbackId) {
-        this.callbackId = callbackId;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
-
-    public Channel getChannel() {
-        return channel;
-    }
-
-    public void setChannel(Channel channel) {
-        this.channel = channel;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -68,31 +26,18 @@ public class MessageActionRequest {
 
         MessageActionRequest that = (MessageActionRequest) o;
 
-        if (actions != null ? !actions.equals(that.actions) : that.actions != null) return false;
-        if (callbackId != null ? !callbackId.equals(that.callbackId) : that.callbackId != null) return false;
-        if (team != null ? !team.equals(that.team) : that.team != null) return false;
-        if (channel != null ? !channel.equals(that.channel) : that.channel != null) return false;
-        return user != null ? user.equals(that.user) : that.user == null;
+        return actions != null ? actions.equals(that.actions) : that.actions == null;
     }
 
     @Override
     public int hashCode() {
-        int result = actions != null ? actions.hashCode() : 0;
-        result = 31 * result + (callbackId != null ? callbackId.hashCode() : 0);
-        result = 31 * result + (team != null ? team.hashCode() : 0);
-        result = 31 * result + (channel != null ? channel.hashCode() : 0);
-        result = 31 * result + (user != null ? user.hashCode() : 0);
-        return result;
+        return actions != null ? actions.hashCode() : 0;
     }
 
     @Override
     public String toString() {
         return "MessageActionRequest{" +
                 "actions=" + actions +
-                ", callbackId='" + callbackId + '\'' +
-                ", team=" + team +
-                ", channel=" + channel +
-                ", user=" + user +
                 '}';
     }
 }
