3ea36d921e4a007709c80136f9bf12b3d71c68cb
Taylor Kemper <tjkemper6@gmail.com>
2017-06-07 22:54:43 -0500

    add architecture outline

---
+++ RiddleBot/src/main/java/io/kemper/AsyncHandler.java
@@ -0,0 +1,18 @@
+package io.kemper;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import io.kemper.api.Response;
+
+import java.util.Map;
+
+public class AsyncHandler implements RequestHandler<Map<String, Object>, Response> {
+
+    @Override
+    public Response handleRequest(Map<String, Object> request, Context context) {
+        context.getLogger().log("Input: " + request);
+
+        return null;
+    }
+
+}
